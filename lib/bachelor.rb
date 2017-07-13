2f735beaa9babe420d679791bc546de330171d04
David Squires <david.squires1@gmail.com>
2017-07-13 21:00:12 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,59 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner_name = ""
+  data[season].each do |contestant|
+    if contestant["status"] == "Winner"
+      winner_name = contestant["name"]
+    end
+  end
+  winner_first_name = winner_name.split(" ")[0]
 end
+
 
 def get_contestant_name(data, occupation)
-  # code here
+  name = ""
+  data.each do |season, contestants|
+    contestants.each do |contestant|
+      if contestant["occupation"] == occupation
+        name = contestant["name"]
+      end
+    end
+  end
+  name
 end
+
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |season, contestants|
+    contestants.each do |contestant|
+      count+=1 if contestant["hometown"] == hometown
+    end
+  end
+  count
 end
+
 
 def get_occupation(data, hometown)
-  # code here
+  occupation = ""
+  data.each do |season, contestants|
+    contestants.each do |contestant|
+      if contestant["hometown"] == hometown
+        occupation = contestant["occupation"]
+        return occupation
+      end
+    end
+  end
 end
 
+
 def get_average_age_for_season(data, season)
-  # code here
+  total_years = 0
+  num_contestants = 0
+  data[season].each do |contestant|
+    total_years = total_years + contestant["age"].to_f
+    num_contestants+=1
+  end
+  (total_years / num_contestants).round
 end
